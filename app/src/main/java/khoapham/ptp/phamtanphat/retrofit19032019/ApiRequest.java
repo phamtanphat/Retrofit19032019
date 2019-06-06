33442a1d7fcd2d09b25a80ec67cf93eca3567536
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-06-06 13:44:07 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/retrofit19032019/ApiRequest.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/retrofit19032019/ApiRequest.java
@@ -1,9 +1,11 @@
 package khoapham.ptp.phamtanphat.retrofit19032019;
 
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface ApiRequest {
     @GET("demo1.json")
-    Call
+    Call<Demo1> getDemo1();
 }
--- app/src/main/java/khoapham/ptp/phamtanphat/retrofit19032019/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/retrofit19032019/MainActivity.java
@@ -2,7 +2,11 @@ package khoapham.ptp.phamtanphat.retrofit19032019;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -19,6 +23,25 @@ public class MainActivity extends AppCompatActivity {
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
         //Khoi tao phuong thuc truy cap ben phia server
+//            Khoi tao ra interface
+        //Khoi tao request
+        ApiRequest apiRequest = retrofit.create(ApiRequest.class);
+        //Goi phuong thuc request len server
+        Call<Demo1> callbackdemo1 = apiRequest.getDemo1();
+        //Lay du lieu thong qua callbackfunction
+        callbackdemo1.enqueue(new Callback<Demo1>() {
+            @Override
+            public void onResponse(Call<Demo1> call, Response<Demo1> response) {
+                Demo1 data = response.body();
+                Log.d("BBB",data.getLogo() + "");
+            }
+
+            @Override
+            public void onFailure(Call<Demo1> call, Throwable t) {
+
+            }
+        });
+
     }
 
 }
